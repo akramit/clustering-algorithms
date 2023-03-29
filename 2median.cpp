041d035784df80631dea1c0be7410a302f6b0999
Amit Roy <amit.akr.roy@gmail.com>
2023-03-29 16:54:16 +0800

    Added Approx Median Algorithm

--- 2median.cpp
+++ 2median.cpp
@@ -101,33 +101,76 @@ int computeOPT2Median(vector<string> &input_strings, vector<string> &cluster1, v
 
 }
 
+int computeApproxOPT2Median(vector<string> &input_strings, vector<string> &a_cluster1, vector<string> &a_cluster2){
+	int approxMedianOjbValue;
+	// Initialize 
+	int i,j,mx_dist=INT_MIN;
+	string s1,s2;
+	//Calculate most separated string
+	for(i=0;i<input_strings.size();i++){
+		for(j=i+1;j<input_strings.size();j++){
+			int currDistance = getDistance(input_strings[i],input_strings[j]);
+			if(currDistance > mx_dist){
+				mx_dist = currDistance;
+				s1=input_strings[i];
+				s2=input_strings[j];
+			}
+		}
+	}
+	a_cluster1.push_back(s1);
+	a_cluster2.push_back(s2);
+
+	// Iterate
+	for(i=0;i<input_strings.size();i++){
+		if(input_strings[i]!= s1 || input_strings[i]!= s2){
+			string center1 = getMedianOfASet(a_cluster1);
+			string center2 = getMedianOfASet(a_cluster2);
+			if(getDistance(input_strings[i],center1) > getDistance(input_strings[i],center2)){
+				a_cluster2.push_back(input_strings[i]);
+			}
+			else{
+				a_cluster1.push_back(input_strings[i]);
+			}
+		}
+	}
+
+	return getMedianObjValue(a_cluster1) + getMedianObjValue(a_cluster2);
+}
+
 int main(){
 
-	vector<string> input_strings{"000","001","010","011","100","101","110","111"};
+	vector<string> input_strings{//"000","001","010","011","100","101","110","111"};
 
-		//"0011000101","0010101010","1111000101","1001010010","1000010100","1000111111","1101001101","0101000011","1011110101","0001101001","0110110001","1001110110","1101001000","1010001001","1001000111","1010001011","0001101000","1101000000","1000111011","0111010001","0100110110"}; 
+		"0011000101","0010101010","1111000101","1001010010","1000010100","1000111111","1101001101","0101000011","1011110101","0001101001","0110110001","1001110110","1101001000","1010001001","1001000111","1010001011","0001101000","1101000000","1000111011","0111010001","0100110110"}; 
 
 	vector<string> cluster1;
 	vector<string> cluster2;
-	int optMedianObjValue;
+	vector<string> a_cluster1;
+	vector<string> a_cluster2;
+	int optMedianObjValue, approxMedianObjValue;
 
 	optMedianObjValue = computeOPT2Median(input_strings,cluster1, cluster2);
+	approxMedianObjValue = computeApproxOPT2Median(input_strings,a_cluster1, a_cluster2);
 
 	cout<<"Optimum Median Objective Value - "<<optMedianObjValue<<endl;
-	cout<<"Cluster 1 "<<endl;
+	cout<<"Approx Median Objective Value - "<<approxMedianObjValue<<endl;
+
+	cout<<"Cluster 1 Centers "<<endl;
 	// print cluster 1
-	for(string s : cluster1)
-		cout<<s<<" ";
-	cout<<endl;
+	//for(string s : cluster1)
+	//	cout<<s<<" ";
+	//cout<<endl;
 	// print center1
-	cout <<getMedianOfASet(cluster1)<<endl;
+	cout <<"OPT Center 1 - "<<getMedianOfASet(cluster1)<<endl;
+	cout <<"Apx Center 1 - "<<getMedianOfASet(a_cluster1)<<endl;
 
 	cout<<"\n Cluster 2 "<<endl;
 	// print cluster 2
-	for(string s : cluster2)
-		cout<<s<<" ";
-	cout<<endl;
+	//for(string s : cluster2)
+	//	cout<<s<<" ";
+	//cout<<endl;
 	// print center2
-	cout <<getMedianOfASet(cluster2)<<endl;
+	cout <<"OPT Center 2 - "<<getMedianOfASet(cluster2)<<endl;
+	cout <<"Apx Center 2 - "<<getMedianOfASet(a_cluster2)<<endl;
 	return 0;
 }
